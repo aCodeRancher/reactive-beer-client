5f4b2d63a578e0425fd060c2f68a6f6d98eb278d
helen <hhma@alumni.cpp.edu>
2021-07-09 13:57:40 -0400

    add two more find beer by id and upc path

--- src/main/java/guru/springframework/reactivebeerclient/config/WebClientProperties.java
+++ src/main/java/guru/springframework/reactivebeerclient/config/WebClientProperties.java
@@ -7,4 +7,6 @@ public class WebClientProperties {
 
     public static final String BASE_URL = "http://api.springframework.guru";
     public static final String BEER_V1_PATH = "/api/v1/beer";
+    public static final String BEER_V1_PATH_beer = "/api/v1/beer/";
+    public static final String BEER_V1_PATH_beerUPC = "/api/v1/beerUpc/";
 }
