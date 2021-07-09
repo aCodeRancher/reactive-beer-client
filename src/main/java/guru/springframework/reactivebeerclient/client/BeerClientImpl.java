edb475969d6bb5b8864109c9aec7ce6e86ef5a76
helen <hhma@alumni.cpp.edu>
2021-07-09 13:58:26 -0400

    implement getBeerById and getBeerByUPC

--- src/main/java/guru/springframework/reactivebeerclient/client/BeerClientImpl.java
+++ src/main/java/guru/springframework/reactivebeerclient/client/BeerClientImpl.java
@@ -23,7 +23,11 @@ public class BeerClientImpl implements BeerClient {
 
     @Override
     public Mono<BeerDto> getBeerById(UUID id, Boolean showInventoryOnHand) {
-        return null;
+       return webClient.get()
+               .uri(uriBuilder -> uriBuilder.path(WebClientProperties.BEER_V1_PATH_beer+id.toString())
+                       .build())
+               .retrieve()
+               .bodyToMono(BeerDto.class);
     }
 
     @Override
@@ -59,6 +63,11 @@ public class BeerClientImpl implements BeerClient {
 
     @Override
     public Mono<BeerDto> getBeerByUPC(String upc) {
-        return null;
+        return webClient.get()
+                .uri(uriBuilder -> uriBuilder.path(WebClientProperties.BEER_V1_PATH_beerUPC+upc)
+                        .build())
+                .retrieve()
+                .bodyToMono(BeerDto.class);
     }
+
 }
