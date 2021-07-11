a6563032f69a6462cad75c8dcf1571afe605c640
helen <hhma@alumni.cpp.edu>
2021-07-11 09:47:06 -0400

    add test for get beer by ID and UPC.

--- src/test/java/guru/springframework/reactivebeerclient/client/BeerClientImplTest.java
+++ src/test/java/guru/springframework/reactivebeerclient/client/BeerClientImplTest.java
@@ -1,12 +1,17 @@
 package guru.springframework.reactivebeerclient.client;
 
 import guru.springframework.reactivebeerclient.config.WebClientConfig;
+import guru.springframework.reactivebeerclient.model.BeerDto;
 import guru.springframework.reactivebeerclient.model.BeerPagedList;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Mono;
 
+import java.util.List;
+import java.util.UUID;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class BeerClientImplTest {
 
@@ -52,7 +57,16 @@ class BeerClientImplTest {
 
     @Test
     void getBeerById() {
-    }
+        Mono<BeerPagedList> pagedListMono = beerClient.listBeers(null, null, null, null,
+                false);
+        BeerPagedList pagedList = pagedListMono.block();
+        BeerDto firstBeer =pagedList.getContent().get(0);
+
+        UUID firstBeerUUID = firstBeer.getId();
+        Mono<BeerDto> beerDtoMono = beerClient.getBeerById(firstBeerUUID,false);
+        BeerDto beerDto = beerDtoMono.block();
+        assertTrue(beerDto.getId().equals(firstBeerUUID));
+     }
 
     @Test
     void createBeer() {
@@ -68,5 +82,16 @@ class BeerClientImplTest {
 
     @Test
     void getBeerByUPC() {
+
+        Mono<BeerPagedList> pagedListMono = beerClient.listBeers(null, null, null, null,
+                null);
+        BeerPagedList pagedList = pagedListMono.block();
+        List<BeerDto> beers =pagedList.toList();
+        BeerDto firstBeer = beers.get(0);
+        String firstBeerUpc = firstBeer.getUpc();
+        Mono<BeerDto> beerDtoMono = beerClient.getBeerByUPC(firstBeerUpc, false);
+        BeerDto beerDto = beerDtoMono.block();
+        assertTrue(beerDto.getUpc().equals(firstBeerUpc));
+
     }
 }
